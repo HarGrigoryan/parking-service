203b2f9c62a14f5f31487fe10b38e3f4f2e3e63d
HarGrigoryan <harut.grigoryan0405@gmail.com>
2025-09-06 14:35:06 +0400

    Implement temporary security configuration for testing purposes

---
+++ src/main/java/com/example/parkingservice/security/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.parkingservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(STATELESS))
+                .authorizeHttpRequests( a->a.anyRequest().permitAll()).build();
+    }
+}
